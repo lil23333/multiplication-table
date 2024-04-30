b41fce9ddb86da9a49ea1dc5ce2e48a5f2c3b053
Valeria <96649604+lil23333@users.noreply.github.com>
2024-04-30 12:24:00 +0300

    Add files via upload

---
+++ index11.js
@@ -0,0 +1,56 @@
+
+let div = document.querySelector('div');
+let p1 = document.querySelector('#p1');
+let p2 = document.querySelector('#p2');
+let p3 = document.querySelector('#p3');
+let p4 = document.querySelector('#p4');
+let p5 = document.querySelector('#p5');
+let p6 = document.querySelector('#p6');
+let p7 = document.querySelector('#p7');
+let p8 = document.querySelector('#p8');
+let p9 = document.querySelector('#p9');
+
+let a1 = 1;
+let a2 = 2;
+let a3 = 3;
+let a4 = 4;
+let a5 = 5;
+let a6 = 6;
+let a7 = 7;
+let a8 = 8;
+let a9 = 9;
+
+p2.innerHTML = ` <pre>
+                  2*1= ${a2*a1} <pre>
+                  2*2= ${a2*a2} <pre>
+                  2*3= ${a2*a3} <pre>
+                  2*4= ${a2*a4} <pre>
+                  2*5= ${a2*a5} <pre>
+                  2*6= ${a2*a6} <pre>
+                  2*7= ${a2*a7} <pre>
+                  2*8= ${a2*a8} <pre>
+                  2*9= ${a2*a9} <pre>
+                  `
+                  
+p3.innerHTML = ` <pre>
+                  3*1= ${a3*a1} <pre>
+                  3*2= ${a3*a2} <pre>
+                  3*3= ${a3*a3} <pre>
+                  3*4= ${a3*a4} <pre>
+                  3*5= ${a3*a5} <pre>
+                  3*6= ${a3*a6} <pre>
+                  3*7= ${a3*a7} <pre>
+                  3*8= ${a3*a8} <pre>
+                  3*9= ${a3*a9} <pre>
+                  `
+p4.innerHTML = ` <pre>
+                  4*1= ${a4*a1} <pre>
+                  4*2= ${a4*a2} <pre>
+                  4*3= ${a4*a3} <pre>
+                  4*4= ${a4*a4} <pre>
+                  4*5= ${a4*a5} <pre>
+                  4*6= ${a4*a6} <pre>
+                  4*7= ${a4*a7} <pre>
+                  4*8= ${a4*a8} <pre>
+                  4*9= ${a4*a9} <pre>
+                  `               
